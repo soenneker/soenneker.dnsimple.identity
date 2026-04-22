3927fa0a9eb10ff29d2c3066d53fbfb775fe4232
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:10 -0500

    TUnit conversion

--- test/Soenneker.DNSimple.Identity.Tests/DNSimpleIdentityUtilTests.cs
+++ test/Soenneker.DNSimple.Identity.Tests/DNSimpleIdentityUtilTests.cs
@@ -1,20 +1,19 @@
-﻿using Soenneker.DNSimple.Identity.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.DNSimple.Identity.Abstract;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.DNSimple.Identity.Tests;
 
-[Collection("Collection")]
-public class DNSimpleIdentityUtilTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class DNSimpleIdentityUtilTests : HostedUnitTest
 {
     private readonly IDNSimpleIdentityUtil _util;
 
-    public DNSimpleIdentityUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public DNSimpleIdentityUtilTests(Host host) : base(host)
     {
         _util = Resolve<IDNSimpleIdentityUtil>(true);
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
     }
